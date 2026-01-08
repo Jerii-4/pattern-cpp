4c2f2c24b5cc5ce421fddfc42bdd4e1dace759a3
Anagha PV <anagharaj2303@gmail.com>
2026-01-08 11:30:10 +0530

    error corrected

--- Array/Easy/rotateD.cpp
+++ Array/Easy/rotateD.cpp
@@ -31,7 +31,7 @@ void rotate(int arr[], int n, int d)
 
     // for (int i = d; i < n; i++)
     // {
-    //     arr[i - d] = arr[i];
+    //     temp[i - d] = arr[i];
     // }
     // for (int i = 0; i < d; i++)
     // {
